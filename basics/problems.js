df50fe2b5cee7180686584459f7e6545684e7e01
Gittyvaibhav <24bee103@nith.ac.in>
2026-03-04 13:29:26 +0530

    added some new code

---
+++ basics/problems.js
@@ -0,0 +1,18 @@
+const product = {
+    name: "Ball Pen (Black)",
+    price: "Rs. 270",
+    inStock: true,
+    ratings: 4.5,
+    offer: "20% off"
+
+};
+
+const profile={
+    username: "vaibhav_123",
+    name: "Vaibhav",
+    posts: 100,
+    followers: 1000,
+    following: 500,
+    isVerified: true,
+    about: "I am a software developer."
+}
